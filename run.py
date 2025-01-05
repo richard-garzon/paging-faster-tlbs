85646c6116065e0d71c3327178f3df4e7d436756
richard garzon <garzon.r@gmail.com>
2025-01-05 01:46:00 -0500

    done i think

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import subprocess
+import sys
 import matplotlib.pyplot as plt
 
-num_trials = 10_000_000
 
 bin_path = "./tlb"
-arg1 = num_trials
+num_trials = int(sys.argv[1])
+powers = int(sys.argv[2])
+
+print("We are running {} trials up to {} pages".format(num_trials, 2**powers))
 
 # start from 2 and go up by powers of 2 to 1028 pages
 num_pages = 2
@@ -12,15 +15,17 @@ num_pages = 2
 x = []
 y = []
 
-for i in range(6):
-    result = subprocess.run([bin_path, str(arg1), str(num_pages)], capture_output=True)
+for i in range(powers):
+    result = subprocess.run(
+        [bin_path, str(num_trials), str(num_pages)], capture_output=True
+    )
     x.append(int(num_pages))
     y.append(float(result.stdout.decode()))
     num_pages *= 2
 
 plt.plot(x, y, marker="o")
 
-plt.xticks(x, x, fontsize="x-small")
+plt.xticks(x, x, fontsize="small")
 plt.xlabel("Number of pages")
 plt.ylabel("Nanoseconds per access")
 
