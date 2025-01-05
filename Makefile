5779666b78f8b7f8079284d1db691b36ee2782c6
richard garzon <garzon.r@gmail.com>
2025-01-04 23:12:52 -0500

    made parameters passable

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	gcc tlb.c -o tlb
+clean:
+	rm tlb
--- tlb.c
+++ tlb.c
@@ -1,18 +1,28 @@
 #include <bits/time.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-int main()
+int main(int argc, char **argv)
 {
+
+	if (argc < 2)
+	{
+		printf("Not enough args passed. First arg is num trials and second is "
+			   "num pages\n");
+	}
+
 	int page_size = 4096;
 	int jump = page_size / sizeof(int);
-	int num_trials = 10000000;
-	int NUMPAGES = 32;
-	int a[NUMPAGES * jump];
+
+	int num_trials = atoi(argv[1]); // 10000000
+	int num_pages = atoi(argv[2]);	// 32
+
+	int a[num_pages * jump];
 
 	// init array to 0 to avoid demand paging showing up in loop
-	memset(a, 0, NUMPAGES * jump);
+	memset(a, 0, num_pages * jump);
 
 	struct timespec start, end;
 
@@ -25,7 +35,7 @@ int main()
 	volatile int j;
 	for (j = 0; j < num_trials; j++)
 	{
-		for (int i = 0; i < NUMPAGES * jump; i += jump)
+		for (int i = 0; i < num_pages * jump; i += jump)
 		{
 			a[i] += 1;
 		}
@@ -41,6 +51,8 @@ int main()
 	double elapsed_ns = time_seconds * 1000000000L + time_nanoseconds;
 	double average_time = elapsed_ns / jump / num_trials;
 
-	printf("Average nanoseconds per access for %d pages: %f\n", NUMPAGES,
+	printf("Average nanoseconds per access for %d pages: %f\n", num_pages,
 		   average_time);
+
+	return 0;
 }
